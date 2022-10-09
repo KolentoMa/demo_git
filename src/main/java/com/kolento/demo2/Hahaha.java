2d9afd5ff2cc4c4ce49a7eea53cec4886f01c9d0
kolentoMa <machao354@icloud.com>
2022-10-09 22:08:39 +0800

    2022-10-9 -branch develop

---
+++ src/main/java/com/kolento/demo2/Hahaha.java
@@ -0,0 +1,9 @@
+package com.kolento.demo2;
+
+public class Hahaha {
+
+    public static void main(String[] args) {
+
+        System.out.println("hahahazhah");
+    }
+}
---
+++ src/main/java/com/kolento/demo2/Test3.java
@@ -0,0 +1,10 @@
+package com.kolento.demo2;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is my branch num1");
+
+    }
+}
