7324212f6e5c4d7baf9106527d4643a5e7f05c03
kolentoMa <machao354@icloud.com>
2022-10-09 21:36:09 +0800

    2022-10-9

---
+++ src/main/java/com/kolento/demo2/Test222.java
@@ -0,0 +1,16 @@
+package com.kolento.demo2;
+
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test222 {
+
+    @Test
+    public void sss(){
+        List<String> list = Arrays.asList("aa","a");
+        System.out.println(list);
+    }
+}
